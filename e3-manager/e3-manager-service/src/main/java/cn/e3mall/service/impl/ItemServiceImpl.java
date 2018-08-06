32486ef57e0b0d63354dd36423eddb64fc4c7149
tianzuo <2719969254@qq.com>
2018-08-06 18:51:23 +0800

    v1.0.13

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -2,10 +2,12 @@ package cn.e3mall.service.impl;
 
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 商品管理Service
@@ -24,16 +26,16 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public TbItem getItemById(long itemId) {
         //根据主键查询
-        TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
-//		TbItemExample example = new TbItemExample();
-//		Criteria criteria = example.createCriteria();
-//		//设置查询条件
-//		criteria.andIdEqualTo(itemId);
-//		//执行查询
-//		List<TbItem> list = itemMapper.selectByExample(example);
-//		if (list != null && list.size() > 0) {
-//			return list.get(0);
-//		}
-        return tbItem;
+        //TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
+        TbItemExample example = new TbItemExample();
+        TbItemExample.Criteria criteria = example.createCriteria();
+        //设置查询条件
+        criteria.andIdEqualTo(itemId);
+        //执行查询
+        List<TbItem> list = itemMapper.selectByExample(example);
+        if (list != null && list.size() > 0) {
+            return list.get(0);
+        }
+        return null;
     }
 }
