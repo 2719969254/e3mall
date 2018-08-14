485aa1627e7d0a27a4b19ea2b796da282324eebc
tianzuo <2719969254@qq.com>
2018-08-14 13:02:40 +0800

    v1.2.04

---
+++ e3-search/e3-search-interface/src/main/java/cn/e3mall/search/service/SearchItemService.java
@@ -0,0 +1,15 @@
+package cn.e3mall.search.service;
+
+import cn.e3mall.common.utils.E3Result;
+
+/**
+ * @author MR.Tian
+ */
+public interface SearchItemService {
+
+	/**
+	 * 实现
+	 * @return
+	 */
+	E3Result importAllItems();
+}
