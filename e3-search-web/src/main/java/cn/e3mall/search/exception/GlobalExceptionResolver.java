0628f739893a41d945f8cee73f25b7c0b5470c22
tianzuo <2719969254@qq.com>
2018-08-16 22:05:49 +0800

    v1.3.1

---
+++ e3-search-web/src/main/java/cn/e3mall/search/exception/GlobalExceptionResolver.java
@@ -0,0 +1,45 @@
+package cn.e3mall.search.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 全局异常处理器
+ *
+ * @author VicterTian
+ * @version V1.0
+ * @Date 2018/8/16
+ */
+public class GlobalExceptionResolver implements HandlerExceptionResolver {
+	private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionResolver.class);
+
+	/**
+	 * 全局异常处理
+	 *
+	 * @param request
+	 * @param response
+	 * @param handler
+	 * @param ex
+	 * @return
+	 */
+	@Override
+	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+		//打印控制台
+		ex.printStackTrace();
+		//写日志
+		LOGGER.debug("测试输出的日志。。。。。。。");
+		LOGGER.info("系统发生异常了。。。。。。。");
+		LOGGER.error("系统发生异常", ex);
+		//发邮件、发短信
+		//使用jmail工具包。发短信使用第三方的Webservice。
+		//显示错误页面
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("error/exception");
+		return modelAndView;
+	}
+}
